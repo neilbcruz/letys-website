a3c2822b2526aff282980ec0a1cc8a2a1b9b25b3
Neil Cruz <neilbcruz@gmail.com>
2023-01-11 13:42:27 -0500

    add new icon position and media query

--- src/components/PageBody/PageBody.js
+++ src/components/PageBody/PageBody.js
@@ -12,9 +12,9 @@ export default function PageBody() {
                     <div className='body__image'>
                         <img className='body__image-one' src={Lety1} alt='Lety with award' />
                         <p>Lorem ipsum dolor sit amet consectetur adipiscing elit dictum libero quam, netus conubia nec semper nascetur nisi curae habitasse. Luctus venenatis tempor hendrerit ridiculus fusce ornare porttitor pulvinar aliquam justo, litora integer cras aenean senectus cursus platea habitant ligula sociosqu, conubia cum arcu quam at elementum molestie montes taciti. Vestibulum libero viverra et condimentum cum convallis leo interdum, iaculis torquent platea conubia duis class volutpat posuere, a dis nascetur mauris magna fringilla urna.</p>
-                        <img className='body__image-two' src={Lety2} alt='Lety with award' />
+                        {/* <img className='body__image-two' src={Lety2} alt='Lety with award' /> */}
                         <p>Lorem ipsum dolor sit amet consectetur adipiscing elit dictum libero quam, netus conubia nec semper nascetur nisi curae habitasse. Luctus venenatis tempor hendrerit ridiculus fusce ornare porttitor pulvinar aliquam justo, litora integer cras aenean senectus cursus platea habitant ligula sociosqu, conubia cum arcu quam at elementum molestie montes taciti. Vestibulum libero viverra et condimentum cum convallis leo interdum, iaculis torquent platea conubia duis class volutpat posuere, a dis nascetur mauris magna fringilla urna.</p>
-                        <img className='body__image-three' src={Lety3} alt='Lety with award' /> 
+                        {/* <img className='body__image-three' src={Lety3} alt='Lety with award' />  */}
                         <p>Lorem ipsum dolor sit amet consectetur adipiscing elit dictum libero quam, netus conubia nec semper nascetur nisi curae habitasse. Luctus venenatis tempor hendrerit ridiculus fusce ornare porttitor pulvinar aliquam justo, litora integer cras aenean senectus cursus platea habitant ligula sociosqu, conubia cum arcu quam at elementum molestie montes taciti. Vestibulum libero viverra et condimentum cum convallis leo interdum, iaculis torquent platea conubia duis class volutpat posuere, a dis nascetur mauris magna fringilla urna.</p>
                     </div>
                 </div>
--- src/components/PageFooter/PageFooter.js
+++ src/components/PageFooter/PageFooter.js
@@ -1,10 +1,9 @@
 import './PageFooter.scss';
-import { FacebookShareButton } from "react-share";
 
 import LetysLogo from '../../assets/images/letys-logo.jpg';
-// import Facebook from '../../assets/icons/facebook.png';
-import FacebookComic from '../../assets/icons/facebook-comic.png';
-import Share from '../../assets/icons/share.png';
+import Facebook from '../../assets/icons/facebook.png';
+import Google from '../../assets/icons/googlemail.png';
+import Phone from '../../assets/icons/telephone.png';
 
 export default function PageFooter() {
     const newTab = url => {
@@ -14,26 +13,27 @@ export default function PageFooter() {
     return (
         <>
             <div className='footer'>
+                <div className='footer__social'>
+                    <div className='footer__social-contact'>
+                        <img onClick={() => window.location = 'mailto:neilbcruz@gmail.com'} src={Google} alt='Google Mail icon that is shaped as letter M and envelope' />
+                        <img onClick={() => newTab('https://www.facebook.com/profile.php?id=100063583611018')} src={Facebook} alt='Facebook icon is a blue circle with a letter f lowercase inside it' />
+                        {/* <a href='tel:+16478642354'>
+                        <img src={Phone} alt='Light blue circle with a phone shape inside it' />
+                        </a> */}
+                        <img onClick={() => window.location = 'tel:+16478642354'} src={Phone} alt='Light blue circle with a phone shape inside it' />
+                    </div>
+                </div>
                 <div className='footer__container'>
                     <div className='footer__text'>
                         <img src={LetysLogo} alt='Yellow Background Letys Name with Coconut' />
                         <h3>© 2022 Lety's Buko Pie</h3>
                     </div>
                     <div className='footer__contact'>
-                        <img onClick={() => newTab('https://www.facebook.com/profile.php?id=100063583611018')} src={FacebookComic} alt='Blue Square with white text letter f' />
-                        <FacebookShareButton
-                            url={"https://www.facebook.com/profile.php?id=100063583611018"}
-                            quote={"facebook share"}
-                            hashtag={"#hashtag"}
-                            description={"aiueo"}
-                            className="footer__contact-share"
-                        >
-                            <img src={Share} alt='Share arrow' />
-                        </FacebookShareButton>
+                        <img src={Google} alt='Google Mail icon that is shaped as letter M and envelope' />
+                        <img src={Facebook} alt='Facebook icon is a blue circle with a letter f lowercase inside it' />
+                        <img src={Phone} alt='Light blue circle with a phone shape inside it' />
                     </div>
-
                 </div>
-
             </div>
         </>
     )
