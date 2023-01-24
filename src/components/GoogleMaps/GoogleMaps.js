05c29040166f08b06b5d52825a6f799558e87a0e
Neil Cruz <neilbcruz@gmail.com>
2023-01-24 14:04:51 -0500

    first commit on branch

--- src/components/GoogleMaps/GoogleMaps.js
+++ src/components/GoogleMaps/GoogleMaps.js
@@ -1,33 +1,79 @@
 import './GoogleMaps.scss';
 
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useState } from "react";
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
 const containerStyle = {
-    width: '100%',
-    height: '30rem'
+    width: '200%',
+    height: '50vh'
 };
 
-const center = {
-    lat: 14.181534560344444,
-    lng: 121.23094523836086
-};
+const markers = [
+    {
+        id: 1,
+        name: "Lety's Buko Pie - Main Store",
+        position: { lat: 14.181547311058496, lng: 121.23095642865529 }
+    },
+    {
+        id: 2,
+        name: "Lety's Buko Pie - Agapita Branch",
+        position: { lat: 14.172583606639034, lng: 121.24338540478081 }
+    },
+    {
+        id: 3,
+        name: "Lety's Buko Pie - Shell Branch",
+        position: { lat: 14.181495274068771, lng: 121.22921137169689 }
+    },
+    {
+        id: 4,
+        name: "Lety's Buko Pie - Pansol Branch",
+        position: { lat: 14.181740807212035, lng: 121.17813891167624 }
+    }
+];
 
 const APIKEY = process.env.API_KEY;
 
 export default function GoogleMaps() {
+    const [activeMarker, setActiveMarker] = useState(null);
+
+    const handleActiveMarker = (marker) => {
+        if (marker === activeMarker) {
+            return;
+        }
+        setActiveMarker(marker);
+    };
+
+    const handleOnLoad = (map) => {
+        const bounds = new window.google.maps.LatLngBounds();
+        markers.forEach(({ position }) => bounds.extend(position));
+        map.fitBounds(bounds);
+    };
 
     return (
         <>
             <LoadScript
-                // googleMapsApiKey={APIKEY}
-                googleMapsApiKey='AIzaSyCfeaOaN3OF4kmuGfRbH6yfmtLbnNE9_Sg'
+                googleMapsApiKey={APIKEY}
+                // googleMapsApiKey='AIzaSyCfeaOaN3OF4kmuGfRbH6yfmtLbnNE9_Sg'
             >
                 <GoogleMap
+                    onLoad={handleOnLoad}
+                    onClick={() => setActiveMarker(null)}
                     mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={20}
+                    zoom={50}
                 >
-                <Marker position={center} />
+                    {markers.map(({ id, name, position }) => (
+                        <Marker
+                            key={id}                            
+                            position={position}
+                            onClick={() => handleActiveMarker(id)}
+                        >
+                            {activeMarker === id ? (
+                                <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+                                    <div>{name}</div>
+                                </InfoWindow>
+                            ) : null}
+                        </Marker>
+                    ))}
                 </GoogleMap>
             </LoadScript>
         </>
