8facf97c080d62ed1d500b30b54da914a6a53b1d
miconficker <mbx@pm.me>
2026-01-06 19:15:18 +0800

    chore: edited index and app.tsx. moved icons and logos to public. removed meta for now.

--- letys-site-v2/src/App.tsx
+++ letys-site-v2/src/App.tsx
@@ -1,48 +1,35 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import PageHeader from './components/PageHeader/PageHeader';
-
 import HomePage from './pages/HomePage/HomePage';
-
 import ProductsPage from './pages/ProductsPage/ProductsPages';
-
 import ProductSpecialty from './components/ProductSpecialty/ProductSpecialty';
 import ProductGoods from './components/ProductGoods/ProductGoods';
 import ProductPasa from './components/ProductPasa/ProductPasa';
-
 import LocationsPage from './pages/LocationsPage/LocationsPage';
 import FaqPage from './pages/FaqPage/FaqPage';
 import ContactPage from './pages/ContactPage/ContactPage';
-
 import PageFooter from './components/PageFooter/PageFooter';
 
 function App() {
   return (
-    <>
-      <div className='app'>
-        <meta charSet="utf-8" />
-        <title>Lety's Buko Pie</title>
-        <link rel="icon" href="./assets/icons/favicon.ico" />
-        {/* <link rel="canonical" href="https://neilbcruz.com/" /> */}
-        <meta name="description" content="Lety's Buko Pie Website" />
-        <Router>
-          <PageHeader />
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/products' element={<ProductsPage />}>
-              <Route path='specialty' element={<ProductSpecialty />} />
-              <Route path='bakedgoods' element={<ProductGoods />} />
-              <Route path='pasalubong' element={<ProductPasa />} />
-            </Route>
-            <Route path='/locations' element={<LocationsPage />} />
-            <Route path='/faq' element={<FaqPage />} />
-            <Route path='/contact' element={<ContactPage />} />
-          </Routes>
-          <PageFooter />
-        </Router>
-      </div>
-    </>
+    <div className='app'>
+      <Router>
+        <PageHeader />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/products' element={<ProductsPage />}>
+            <Route path='specialty' element={<ProductSpecialty />} />
+            <Route path='bakedgoods' element={<ProductGoods />} />
+            <Route path='pasalubong' element={<ProductPasa />} />
+          </Route>
+          <Route path='/locations' element={<LocationsPage />} />
+          <Route path='/faq' element={<FaqPage />} />
+          <Route path='/contact' element={<ContactPage />} />
+        </Routes>
+        <PageFooter />
+      </Router>
+    </div>
   );
 }
 
