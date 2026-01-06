bb299745665096ed5c39cb45b03e721024199c77
miconficker <mbx@pm.me>
2026-01-06 21:30:12 +0800

    fix: web-vitals to typescript

--- src/reportWebVitals.tsx
+++ src/reportWebVitals.tsx
@@ -1,13 +1,13 @@
 import type { Metric } from 'web-vitals';
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+import { onCLS, onINP, onFCP, onLCP, onTTFB } from 'web-vitals';
 
 export function reportWebVitals(onPerfEntry?: (metric: Metric) => void) {
   if (onPerfEntry && typeof onPerfEntry === 'function') {
-    getCLS(onPerfEntry);
-    getFID(onPerfEntry);
-    getFCP(onPerfEntry);
-    getLCP(onPerfEntry);
-    getTTFB(onPerfEntry);
+    onCLS(onPerfEntry);
+    onINP(onPerfEntry);
+    onFCP(onPerfEntry);
+    onLCP(onPerfEntry);
+    onTTFB(onPerfEntry);
   }
 }
 
